2292ff1fdf5190b5301e2fd8890f62929e344c64
Oleksandr Lebediev <san.lebed1998@gmail.com>
2022-07-20 22:51:46 +0300

    update #10

--- js/task-10.js
+++ js/task-10.js
@@ -2,43 +2,56 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const refs ={
+  blockCounterEl: document.querySelector('input'),
+  createBtnEl: document.querySelector('button[data-create]'),
+  destroyBtnEl: document.querySelector('button[data-destroy]'),
+  blockWrapEl:document.querySelector('#boxes')
+}
 
-const blockCounterEl = document.querySelector('input')
-const createBtnEl = document.querySelector('button[data-create]')
-const destroyBtnEl = document.querySelector('button[data-destroy]')
-const blockWrapEl = document.querySelector('#boxes')
-blockWrapEl.style.display = 'flex'
-blockWrapEl.style.width = '600px'
-blockWrapEl.style.flexWrap = 'wrap'
-blockWrapEl.style.justifyContent = 'space-between'
-blockWrapEl.style.alignItems = 'center'
+refs.blockWrapEl.style.display = 'flex'
+refs.blockWrapEl.style.width = '600px'
+refs.blockWrapEl.style.flexWrap = 'wrap'
+refs.blockWrapEl.style.justifyContent = 'space-between'
+refs.blockWrapEl.style.alignItems = 'center'
+
+const startedData = {
+  array: [],
+  width: 20,
+  height: 20,
+  costumerValue:0
+}
 
 
 
+refs.blockCounterEl.addEventListener('blur', (event) => {
+  startedData.costumerValue = Number(event.currentTarget.value);
+})
 
-blockCounterEl.addEventListener('blur', (event) => {
-  let outputData = event.currentTarget.value;
-  let array = [];
-  let width = 20;
-  let height = 20;
-
-    function createBoxes(amount) {
-      for (let i = 0; i < amount; i++){
-        array[i] = document.createElement('div');
-        array[i].style.width = `${width += 10}px`;
-        array[i].style.height = `${height += 10}px`;
-        array[i].style.backgroundColor = getRandomHexColor();
+  function createBoxes() {
+    if (refs.blockWrapEl.children.length !== 0) {
+      refs.blockWrapEl.textContent = ""
+    }
+    
+    const activeObj = { ...startedData }
+    
+    for (let i = 0; i < activeObj.costumerValue; i++){
+      
+        activeObj.array[i] = document.createElement('div');
+        activeObj.array[i].style.width = `${activeObj.width += 10}px`;
+        activeObj.array[i].style.height = `${activeObj.height += 10}px`;
+        activeObj.array[i].style.backgroundColor = getRandomHexColor();
       }
-      blockWrapEl.append(...array)
-      amount=0
+    refs.blockWrapEl.append(...activeObj.array);
+    console.log( refs.blockWrapEl.children.length)
+    activeObj = { ...startedData };
     }
   
-  createBtnEl.addEventListener('click',createBoxes(outputData))
-  destroyBtnEl.addEventListener('click',()=> {
-    blockWrapEl.textContent = "";
-    blockCounterEl.textContent = "";
-  })
-   
+refs.createBtnEl.addEventListener('click', createBoxes)  
 
-})
- 
+refs.destroyBtnEl.addEventListener('click', clearBoxes)
+
+function clearBoxes() {
+    refs.blockWrapEl.textContent = "";
+    refs.blockCounterEl.value = "";
+  }
